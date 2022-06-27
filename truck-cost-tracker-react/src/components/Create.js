f846da0cf905ed535f9d8033de8faf80f75b106a
christian-fortin <christian.ep.410@gmail.com>
2022-06-27 18:54:51 -0400

    front end set up to talk to back end partially

--- truck-cost-tracker-react/src/components/Create.js
+++ truck-cost-tracker-react/src/components/Create.js
@@ -5,31 +5,30 @@ import axios from 'axios';
 
 const Create = () => {
 
-    // I think I have seen this format before when you need to handle multiple states. -- 410
-  //  const state = {
-  //       VIN: '',
-  //       distance: '',
-  //       tolls: '',
-  //       foodBudget: '',
-  //       wearAndTear: '',
-  //       misc: '',
-  //       startingPoint: '',
-  //       endingPoint: ''
-  //   }
 
-    // const  handleInput = (e) => {
-    //     this.setState({
-    //         [e.target.name]: e.target.value
-    //     })
-    // }
 
-    // const saveTripCost = async(e) => {
-    //     e.preventDefault();
 
-    //     const res = await axios.post('/api/addTrip', this.state);
+  const [vin, setVin] = useState('')
+  const [distance, setDistance] = useState('')
+  const [tolls, setTolls] = useState('')
+  const [foodBudget, setFoodBudget] = useState('')
+  const [wearAndTear, setWearAndTear] = useState('')
+  const [misc, setMisc] = useState('')
+  const [startingPoint, setStartingPoint] = useState('')
+  const [endingPoint, setEndingPoint] = useState('')
 
-    // }
 
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(vin);
+    console.log(distance);
+    console.log(tolls);
+    console.log(foodBudget);
+    console.log(wearAndTear);
+    console.log(misc);
+    console.log(startingPoint);
+    console.log(endingPoint);
+}
 
 
 
@@ -40,15 +39,14 @@ const Create = () => {
       </div>
 
       {/* FORM FOR VIN# ======== LINK THIS TO THE API*/}
-      {/* THINKING: There shouldn't be an action because we don't want to send it anywhere else. There should be two forms on this page, one form to input the data of the truck, a second to input the data of the trip? */}
+
 <div id='formDiv'>
-      <form action="XXX" method="post" /* onSubmit={this.saveTripCost}*/> 
+      <form onSubmit={handleSubmit}> 
         <div className='spaceAboveAndBelow'>
-          <label className="labelText" for="VIN">Enter the VIN#</label>
-          {/* what does the "this" refer to? --410 */}
-          <input className='createPageInput' type="text"  /*onChange={this.handleInput} value={this.state.VIN} */id="VIN" name="VIN" />
+          <label className="labelText" htmlFor="VIN">Enter the VIN#</label>
+          <input className='createPageInput' type="text"  onChange={e => setVin(e.target.value)} value={vin} id="VIN" name="VIN" />
         </div>
-        {/* <button type="submit">Submit</button> */}
+
 
 
 {/* MAKE MODEL TANK MPG */}
@@ -66,41 +64,41 @@ const Create = () => {
 {/* COST CALCULATOR SECTION OF FORM */}
  
         <div className='spaceAboveAndBelow'>
-          <label className="labelText" for="distance">Enter the Distance(Mi)</label>
-          <input className='createPageInput' type="number" /*onChange={this.handleInput} value={this.state.distance} */ id="distance" name="distance" />
+          <label className="labelText" htmlFor="distance">Enter the Distance(Mi)</label>
+          <input className='createPageInput' type="number" onChange={e => setDistance(e.target.value)} value={distance} id="distance" name="distance" />
         </div>
 
         <div className='spaceAboveAndBelow'>
-          <label className="labelText" for="tolls">Enter the tolls</label>
-          <input className='createPageInput' type="number" /*onChange={this.handleInput} value={this.state.tolls} */ id="tolls" name="tolls" />
+          <label className="labelText" htmlFor="tolls">Enter the tolls</label>
+          <input className='createPageInput' type="number" onChange={e => setTolls(e.target.value)} value={tolls} id="tolls" name="tolls" />
         </div>
 
         <div className='spaceAboveAndBelow'>
-          <label className="labelText" for="foodBudget">Enter the Food Budget</label>
-          <input className='createPageInput' type="number" /*onChange={this.handleInput} value={this.state.foodBudget} */ id="foodBudget" name="foodBudget" />
+          <label className="labelText" htmlFor="foodBudget">Enter the Food Budget</label>
+          <input className='createPageInput' type="number" onChange={e => setFoodBudget(e.target.value)} value={foodBudget} id="foodBudget" name="foodBudget" />
         </div>
 
         <div className='spaceAboveAndBelow'>
-          <label className="labelText" for="wearAndTear">Enter wear and tear budget</label>
-          <input  className='createPageInput' type="number" /*onChange={this.handleInput} value={this.state.wearAndTear}*/ id="wearAndTear" name="wearAndTear" />
+          <label className="labelText" htmlFor="wearAndTear">Enter wear and tear budget</label>
+          <input  className='createPageInput' type="number" onChange={e => setWearAndTear(e.target.value)} value={wearAndTear} id="wearAndTear" name="wearAndTear" />
         </div>
 
         <div className='spaceAboveAndBelow'>
-          <label className="labelText" for="misc">Enter the miscellaneous expenses</label>
-          <input className='createPageInput' type="number" /*onChange={this.handleInput} value={this.state.misc} */id="misc" name="misc" />
+          <label className="labelText" htmlFor="misc">Enter the miscellaneous expenses</label>
+          <input className='createPageInput' type="number" onChange={e => setMisc(e.target.value)} value={misc} id="misc" name="misc" />
         </div>
 
         <div className='spaceAboveAndBelow'>
-          <label className="labelText" for="startingPoint">Starting Point: </label>
-          <input className='createPageInput' type="text" /*onChange={this.handleInput} value={this.state.startingPoint} */id="startingPoint" name="startingPoint" />
+          <label className="labelText" htmlFor="startingPoint">Starting Point: </label>
+          <input className='createPageInput' type="text" onChange={e => setStartingPoint(e.target.value)} value={startingPoint}  id="startingPoint" name="startingPoint" />
         </div>
 
         <div className='spaceAboveAndBelow'>
-          <label className="labelText" for="endingPoint">End Point: </label>
-          <input className='createPageInput' type="text" /*onChange={this.handleInput} value={this.state.endingPoint}*/ id="endingPoint" name="endingPoint" />
+          <label className="labelText" htmlFor="endingPoint">End Point: </label>
+          <input className='createPageInput' type="text"  onChange={e => setEndingPoint(e.target.value)} value={endingPoint} id="endingPoint" name="endingPoint" />
         </div>
 
-        <button id='submitButton' type="submit">Submit For Calculations</button>
+        <input type="submit" value="Submit For Calculations" id='submitButton'/>
       </form>
       </div>
 
--- truck-cost-tracker-react/src/components/Home.js
+++ truck-cost-tracker-react/src/components/Home.js
@@ -9,18 +9,23 @@ const Home = () => {
       </div>
       <div id="tableDiv">
         <table>
+          <thead>
           <tr>
             <th>ID</th>
             <th>Trip Name</th>
             <th>Cost</th>
             <th>Date</th>
           </tr>
+          </thead>
+          <tbody>
           <tr>
             <td>1</td>
             <td>Trip to Mexico</td>
             <td>$400</td>
             <td>MM/DD/YYYY HH:MM:SS</td>
           </tr>
+          </tbody>
+          <tfoot></tfoot>
         </table>
       </div>
     </div>
