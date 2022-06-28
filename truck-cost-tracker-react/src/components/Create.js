c12f47fc1bc590e1a1ee5a8d63e7462041dbfea4
christian-fortin <christian.ep.410@gmail.com>
2022-06-28 08:06:43 -0400

    slight updates to CSS and some parts added in create

--- truck-cost-tracker-react/src/components/Create.js
+++ truck-cost-tracker-react/src/components/Create.js
@@ -7,7 +7,7 @@ const Create = () => {
 
 
 
-
+  const [name, setName] = useState('')
   const [vin, setVin] = useState('')
   const [distance, setDistance] = useState('')
   const [tolls, setTolls] = useState('')
@@ -20,6 +20,7 @@ const Create = () => {
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
+    console.log(name);
     console.log(vin);
     console.log(distance);
     console.log(tolls);
@@ -38,10 +39,19 @@ const Create = () => {
         <h1 id="createTitleH1">Create a Trip</h1>
       </div>
 
+    
+
       {/* FORM FOR VIN# ======== LINK THIS TO THE API*/}
 
 <div id='formDiv'>
       <form onSubmit={handleSubmit}> 
+
+
+      <div className='spaceAboveAndBelow'>
+          <label className="labelText" htmlFor="name">Enter the Trip Name</label>
+          <input className='createPageInput' type="name"  onChange={e => setName(e.target.value)} value={name} id="name" name="name" />
+          </div>
+          
         <div className='spaceAboveAndBelow'>
           <label className="labelText" htmlFor="VIN">Enter the VIN#</label>
           <input className='createPageInput' type="text"  onChange={e => setVin(e.target.value)} value={vin} id="VIN" name="VIN" />
@@ -65,44 +75,44 @@ const Create = () => {
  
         <div className='spaceAboveAndBelow'>
           <label className="labelText" htmlFor="distance">Enter the Distance(Mi)</label>
-          <input className='createPageInput' type="number" onChange={e => setDistance(e.target.value)} value={distance} id="distance" name="distance" />
+          <input className='createPageInput' type="text" onChange={e => setDistance(e.target.value)} value={distance} id="distance" name="distance" />
         </div>
 
         <div className='spaceAboveAndBelow'>
           <label className="labelText" htmlFor="tolls">Enter the tolls</label>
-          <input className='createPageInput' type="number" onChange={e => setTolls(e.target.value)} value={tolls} id="tolls" name="tolls" />
+          <input className='createPageInput' type="text" onChange={e => setTolls(e.target.value)} value={tolls} id="tolls" name="tolls" />
         </div>
 
         <div className='spaceAboveAndBelow'>
           <label className="labelText" htmlFor="foodBudget">Enter the Food Budget</label>
-          <input className='createPageInput' type="number" onChange={e => setFoodBudget(e.target.value)} value={foodBudget} id="foodBudget" name="foodBudget" />
+          <input className='createPageInput' type="text" onChange={e => setFoodBudget(e.target.value)} value={foodBudget} id="foodBudget" name="foodBudget" />
         </div>
 
         <div className='spaceAboveAndBelow'>
           <label className="labelText" htmlFor="wearAndTear">Enter wear and tear budget</label>
-          <input  className='createPageInput' type="number" onChange={e => setWearAndTear(e.target.value)} value={wearAndTear} id="wearAndTear" name="wearAndTear" />
+          <input  className='createPageInput' type="text" onChange={e => setWearAndTear(e.target.value)} value={wearAndTear} id="wearAndTear" name="wearAndTear" />
         </div>
 
         <div className='spaceAboveAndBelow'>
           <label className="labelText" htmlFor="misc">Enter the miscellaneous expenses</label>
-          <input className='createPageInput' type="number" onChange={e => setMisc(e.target.value)} value={misc} id="misc" name="misc" />
+          <input className='createPageInput' type="text" onChange={e => setMisc(e.target.value)} value={misc} id="misc" name="misc" />
         </div>
 
         <div className='spaceAboveAndBelow'>
           <label className="labelText" htmlFor="startingPoint">Starting Point: </label>
-          <input className='createPageInput' type="text" onChange={e => setStartingPoint(e.target.value)} value={startingPoint}  id="startingPoint" name="startingPoint" />
+          <input className='createPageInput startEndPointInputs' type="text" onChange={e => setStartingPoint(e.target.value)} value={startingPoint}  id="startingPoint" name="startingPoint" />
         </div>
 
         <div className='spaceAboveAndBelow'>
           <label className="labelText" htmlFor="endingPoint">End Point: </label>
-          <input className='createPageInput' type="text"  onChange={e => setEndingPoint(e.target.value)} value={endingPoint} id="endingPoint" name="endingPoint" />
+          <input className='createPageInput startEndPointInputs' type="text"  onChange={e => setEndingPoint(e.target.value)} value={endingPoint} id="endingPoint" name="endingPoint" />
         </div>
 
         <input type="submit" value="Submit For Calculations" id='submitButton'/>
       </form>
       </div>
 
-      <h3 id='tripCostLowerTitle'>Trip Cost:</h3>
+      <h3 id='tripCostLowerTitle'>Trip Calculation:</h3>
       <p> (Distance / (MPG * Gas Price)) + Tolls + Food + Wear and Tear + Other</p>
 
       <p><strong>Actual inputs go here, the above is just a breakdown of the formula</strong></p>
