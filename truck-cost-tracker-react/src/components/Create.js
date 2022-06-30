ed10f5cfbe684f93a4cd3bf4d499a8f7a5a03b9b
christian-fortin <christian.ep.410@gmail.com>
2022-06-30 07:56:41 -0400

    linked the vin# api and the calculations. Linked the data from the api to the table

--- truck-cost-tracker-react/src/components/Create.js
+++ truck-cost-tracker-react/src/components/Create.js
@@ -4,67 +4,123 @@ import { useState } from 'react';
 import axios from 'axios';
 
 const Create = () => {
-
-  const [name, setName] = useState('')
-  const [vin, setVin] = useState('')
-  const [distance, setDistance] = useState('')
-  const [tolls, setTolls] = useState('')
-  const [foodBudget, setFoodBudget] = useState('')
-  const [wearAndTear, setWearAndTear] = useState('')
-  const [misc, setMisc] = useState('')
-  const [startingPoint, setStartingPoint] = useState('')
-  const [endingPoint, setEndingPoint] = useState('')
-
-
-  useEffect(()=> {
-    // make external api call in here. 
-    // set data
-    // make sure vin is valid
+  /* DATA STATES
+=============== ========================= ========================= ========================= ========================= ====== =================== ========================= ===*/
+  const [name, setName] = useState('');
+  const [vin, setVin] = useState('');
+  const [distance, setDistance] = useState('');
+  const [tolls, setTolls] = useState('');
+  const [foodBudget, setFoodBudget] = useState('');
+  const [wearAndTear, setWearAndTear] = useState('');
+  const [misc, setMisc] = useState('');
+  const [startingPoint, setStartingPoint] = useState('');
+  const [endingPoint, setEndingPoint] = useState('');
+  /* ============== ============= ============= ============= ==================== ===================== ===================== ===================== ===================== ===========*/
 
 
-  }, [vin])
 
 
+
+  /* THIS IS HOW WE POST THE TRIPS FROM THE FORM
+=============== ============== ============== ============== ============== ============== ============== ============== ============== ============== ============== =============*/
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    // console.log(name);
-    // console.log(vin);
-    // console.log(distance);
-    // console.log(tolls);
-    // console.log(foodBudget);
-    // console.log(wearAndTear);
-    // console.log(misc);
-    // console.log(startingPoint);
-    // console.log(endingPoint);
+
+    fetch('http://127.0.0.1:8000/api/postTrips', {
+      method: 'POST',
+      body: JSON.stringify({
+        trip_name: name,
+        vin_number: vin,
+        distance: distance,
+        tolls: tolls,
+        foodBudget: foodBudget,
+        wearAndTear: wearAndTear,
+        misc: misc,
+        startingPoint: startingPoint,
+        endingPoint: endingPoint,
+      }),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((r) => r.json())
+      // .then(r => console.log(r))
+      .catch((err) => console.log(err));
+  };
+  /* ============ ============ ============ ============ ============ ============ ============ ============ ============ ============ ============ =========== =========== =============*/
 
 
-    // THIS IS HOW WE GET THE PETS
-// fetch ('http://127.0.0.1:8000/api/getPets')
-// .then(r => r.json()) 
-// .then(r => console.log(r))
-// .catch(err => console.log(err))
-
-  // THIS IS HOW WE POST THE PETS FROM THE FORM
-fetch ('http://127.0.0.1:8000/api/postPet', {
-  method: 'POST',
-  body: JSON.stringify({
-    name: vin,
-  }),
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-},
-
-})
-.then(r => r.json()) 
-.then(r => console.log(r))
-.catch(err => console.log(err))
 
 
-    // function calculateCost() {
-    //  let totalCost =  (distance / (MPG * GasPrice)) + tolls + foodBudget + wearAndTear + misc
-    // }
-}
+
+
+
+  /* THIS IS TO GET ALL THE DATA FOR THE TRIPS -- USED FOR THE CALCULATIONS AT THE BOTTOM
+================= ================ ================ ================ ================ ================ ================ ================ ================ ================ =======*/
+  const [data, setData] = useState([]);
+  let getData = async () => {
+    let trips = await fetch('http://127.0.0.1:8000/api/getTrips');
+    let json = await trips.json();
+    if (json) {
+      setData(json);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  /* ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ============ ============*/
+
+
+
+
+
+
+  /* THIS IS THE CALCULATION FOR THE TOTAL
+  ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ============ ============*/
+  // let total = ((data[data.length-1].distance / (1 * 1)) + data[data.length-1].tolls + data[data.length-1].foodBudget + data[data.length-1].wearAndTear + data[data.length-1].misc)
+  // console.log(total);
+  /* ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ============ ============*/
+
+
+
+
+
+
+  /*THIS IS HOW WE GET THE VIN INFO
+  ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ============ ============*/
+  // useEffect(()=> {
+  //   // THIS IS HOW WE GET THE VIN INFO
+  //   fetch (`https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/${vin}?format=json`)
+  //   .then(r => r.json())
+  //   .then(r => console.log(r))
+  //   .catch(err => console.log(err))
+  // }, [])
+
+  const [vinData, setVinData] = useState([]);
+  let getVinData = async () => {
+    let vinNumber = await fetch (`https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/WVGBV75N19W507096?format=json`)
+    let json = await vinNumber.json();
+    if (json) {
+      setData(json);
+    }
+  };
+
+  useEffect(() => {
+    getVinData();
+  }, []);
+
+  console.log(vinData);
+
+
+// HOW TO CATCH THE DATA I NEED:
+// MAKE:
+// MODEL:
+// TANK SIZE:
+// MPG:
+  /* ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ========= ============ ============*/
+
 
 
 
@@ -73,100 +129,177 @@ fetch ('http://127.0.0.1:8000/api/postPet', {
 
 
   return (
-    <div id='createComponentContainer'>
+    <div id="createComponentContainer">
       <div id="createTitleDiv">
         <h1 id="createTitleH1">Create a Trip</h1>
       </div>
 
-    
-
       {/* FORM FOR VIN# ======== LINK THIS TO THE API*/}
 
-<div id='formDiv'>
-      <form onSubmit={handleSubmit}> 
-
-
-      <div className='spaceAboveAndBelow'>
-          <label className="labelText" htmlFor="name">Enter the Trip Name</label>
-          <input className='createPageInput' type="name"  onChange={e => setName(e.target.value)} value={name} id="name" name="name" />
+      <div id="formDiv">
+        <form onSubmit={handleSubmit}>
+          <div className="spaceAboveAndBelow">
+            <label className="labelText" htmlFor="name">
+              Enter the Trip Name
+            </label>
+            <input
+              className="createPageInput"
+              type="name"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              id="name"
+              name="name"
+            />
           </div>
 
-        <div className='spaceAboveAndBelow'>
-          <label className="labelText" htmlFor="vin">Enter the VIN#</label>
-          <input className='createPageInput' type="text"  onChange={e => setVin(e.target.value)} value={vin} id="vin" name="vin" />
-        </div>
+          <div className="spaceAboveAndBelow">
+            <label className="labelText" htmlFor="vin">
+              Enter the VIN#
+            </label>
+            <input
+              className="createPageInput"
+              type="text"
+              onChange={(e) => setVin(e.target.value)}
+              value={vin}
+              id="vin"
+              name="vin"
+            />
+          </div>
 
+          {/* MAKE MODEL TANK MPG */}
+          <div className="spaceAboveAndBelow" id="VINOutput">
+            <div className="VinOutputItem">MAKE: XXX</div>
+            <div className="VinOutputItem">MODEL: YYY</div>
+            <div className="VinOutputItem">TANK SIZE: ZZZ</div>
+            <div className="VinOutputItem">MPG: AAA</div>
+          </div>
 
+          <div id="createSubTitleDiv">
+            <h2 id="createSubTitleH2">Cost Calculator:</h2>
+          </div>
 
+          {/* COST CALCULATOR SECTION OF FORM */}
 
-{/* MAKE MODEL TANK MPG */}
-      <div className='spaceAboveAndBelow' id='VINOutput'>
-        <div className='VinOutputItem'>MAKE: XXX</div>
-        <div className='VinOutputItem'>MODEL: YYY</div>
-        <div className='VinOutputItem'>TANK SIZE: ZZZ</div>
-        <div className='VinOutputItem'>MPG: AAA</div>
+          <div className="spaceAboveAndBelow">
+            <label className="labelText" htmlFor="distance">
+              Enter the Distance(Mi)
+            </label>
+            <input
+              className="createPageInput"
+              type="text"
+              onChange={(e) => setDistance(e.target.value)}
+              value={distance}
+              id="distance"
+              name="distance"
+            />
+          </div>
+
+          <div className="spaceAboveAndBelow">
+            <label className="labelText" htmlFor="tolls">
+              Enter the tolls
+            </label>
+            <input
+              className="createPageInput"
+              type="text"
+              onChange={(e) => setTolls(e.target.value)}
+              value={tolls}
+              id="tolls"
+              name="tolls"
+            />
+          </div>
+
+          <div className="spaceAboveAndBelow">
+            <label className="labelText" htmlFor="foodBudget">
+              Enter the Food Budget
+            </label>
+            <input
+              className="createPageInput"
+              type="text"
+              onChange={(e) => setFoodBudget(e.target.value)}
+              value={foodBudget}
+              id="foodBudget"
+              name="foodBudget"
+            />
+          </div>
+
+          <div className="spaceAboveAndBelow">
+            <label className="labelText" htmlFor="wearAndTear">
+              Enter wear and tear budget
+            </label>
+            <input
+              className="createPageInput"
+              type="text"
+              onChange={(e) => setWearAndTear(e.target.value)}
+              value={wearAndTear}
+              id="wearAndTear"
+              name="wearAndTear"
+            />
+          </div>
+
+          <div className="spaceAboveAndBelow">
+            <label className="labelText" htmlFor="misc">
+              Enter the miscellaneous expenses
+            </label>
+            <input
+              className="createPageInput"
+              type="text"
+              onChange={(e) => setMisc(e.target.value)}
+              value={misc}
+              id="misc"
+              name="misc"
+            />
+          </div>
+
+          <div className="spaceAboveAndBelow">
+            <label className="labelText" htmlFor="startingPoint">
+              Starting Point:{' '}
+            </label>
+            <input
+              className="createPageInput startEndPointInputs"
+              type="text"
+              onChange={(e) => setStartingPoint(e.target.value)}
+              value={startingPoint}
+              id="startingPoint"
+              name="startingPoint"
+            />
+          </div>
+
+          <div className="spaceAboveAndBelow">
+            <label className="labelText" htmlFor="endingPoint">
+              End Point:{' '}
+            </label>
+            <input
+              className="createPageInput startEndPointInputs"
+              type="text"
+              onChange={(e) => setEndingPoint(e.target.value)}
+              value={endingPoint}
+              id="endingPoint"
+              name="endingPoint"
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Submit For Calculations"
+            id="submitButton"
+          />
+        </form>
       </div>
 
-      <div id="createSubTitleDiv">
-        <h2 id="createSubTitleH2">Cost Calculator:</h2>
-      </div>
+      <h3 id="tripCostLowerTitle">Trip Calculation:</h3>
+      <p>
+        {' '}
+        (Distance / (MPG * Gas Price)) + Tolls + Food + Wear and Tear + Other
+      </p>
 
-{/* COST CALCULATOR SECTION OF FORM */}
- 
-        <div className='spaceAboveAndBelow'>
-          <label className="labelText" htmlFor="distance">Enter the Distance(Mi)</label>
-          <input className='createPageInput' type="text" onChange={e => setDistance(e.target.value)} value={distance} id="distance" name="distance" />
-        </div>
-
-        <div className='spaceAboveAndBelow'>
-          <label className="labelText" htmlFor="tolls">Enter the tolls</label>
-          <input className='createPageInput' type="text" onChange={e => setTolls(e.target.value)} value={tolls} id="tolls" name="tolls" />
-        </div>
-
-        <div className='spaceAboveAndBelow'>
-          <label className="labelText" htmlFor="foodBudget">Enter the Food Budget</label>
-          <input className='createPageInput' type="text" onChange={e => setFoodBudget(e.target.value)} value={foodBudget} id="foodBudget" name="foodBudget" />
-        </div>
-
-        <div className='spaceAboveAndBelow'>
-          <label className="labelText" htmlFor="wearAndTear">Enter wear and tear budget</label>
-          <input  className='createPageInput' type="text" onChange={e => setWearAndTear(e.target.value)} value={wearAndTear} id="wearAndTear" name="wearAndTear" />
-        </div>
-
-        <div className='spaceAboveAndBelow'>
-          <label className="labelText" htmlFor="misc">Enter the miscellaneous expenses</label>
-          <input className='createPageInput' type="text" onChange={e => setMisc(e.target.value)} value={misc} id="misc" name="misc" />
-        </div>
-
-        <div className='spaceAboveAndBelow'>
-          <label className="labelText" htmlFor="startingPoint">Starting Point: </label>
-          <input className='createPageInput startEndPointInputs' type="text" onChange={e => setStartingPoint(e.target.value)} value={startingPoint}  id="startingPoint" name="startingPoint" />
-        </div>
-
-        <div className='spaceAboveAndBelow'>
-          <label className="labelText" htmlFor="endingPoint">End Point: </label>
-          <input className='createPageInput startEndPointInputs' type="text"  onChange={e => setEndingPoint(e.target.value)} value={endingPoint} id="endingPoint" name="endingPoint" />
-        </div>
-
-        <input type="submit" value="Submit For Calculations" id='submitButton'/>
-      </form>
-      </div>
-
-      <h3 id='tripCostLowerTitle'>Trip Calculation:</h3>
-      <p> (Distance / (MPG * Gas Price)) + Tolls + Food + Wear and Tear + Other</p>
-
-      <p><strong>Actual inputs go here, the above is just a breakdown of the formula</strong></p>
-      <h1>TOTAL HERE</h1>
-     
-
-
-
-
-
-
-
-
-
+      {/* <p>
+        {' '}
+        ({data[data.length - 1].distance} / (MPG * Gas Price)) +{' '}
+        {data[data.length - 1].tolls} + {data[data.length - 1].foodBudget} +{' '}
+        {data[data.length - 1].wearAndTear} + {data[data.length - 1].misc}
+      </p> */}
+      {/* <h1>TOTAL: ${total}</h1> */}
+      <h1>TOTAL: $</h1>
     </div>
   );
 };
--- truck-cost-tracker-react/src/components/Home.js
+++ truck-cost-tracker-react/src/components/Home.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 
 const Home = () => {
+    /* THIS IS HOW WE GET THE TRIPS FROM THE DATABASE
+============ ========== ================== ============= ================================== ============ ========================== ================ ============= ==============*/
+  const [data, setData] = useState([]);
+  let getData = async () => {
+    let trips = await fetch('http://127.0.0.1:8000/api/getTrips');
+    let json = await trips.json();
+    if (json) {
+      setData(json);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+   /* ================= ============== ==================== ================================== ==================================== ==================== ============ ===============*/
+
+
+
+  // DATA of trips is in the variable data
+  console.log(data);
+
   return (
     <div>
       <div id="homeTitleDiv">
@@ -10,20 +30,41 @@ const Home = () => {
       <div id="tableDiv">
         <table>
           <thead>
-          <tr>
-            <th>ID</th>
-            <th>Trip Name</th>
-            <th>Cost</th>
-            <th>Date</th>
-          </tr>
+            <tr>
+              <th>ID</th>
+              <th>Trip Name</th>
+              <th>Cost</th>
+              <th>Date</th>
+              <th>vin</th>
+              <th>distance</th>
+              <th>tolls</th>
+              <th>foodBudget</th>
+              <th>wearAndTear</th>
+              <th>misc</th>
+              <th>startingPoint</th>
+              <th>endingPoint</th>
+            </tr>
           </thead>
           <tbody>
-          <tr>
-            <td>1</td>
-            <td>Trip to Mexico</td>
-            <td>$400</td>
-            <td>MM/DD/YYYY HH:MM:SS</td>
-          </tr>
+            {data.map((datum) => {
+              return (
+                <tr key={datum.id}>
+                  <td className='datumSquare_table'>{datum.id}</td>
+                  <td className='datumSquare_table'>{datum.trip_name}</td>
+                  <td className='datumSquare_table'>$cost</td>
+                  <td className='datumSquare_table'>MM/DD/YYYY HH:MM:SS</td>
+                  <td className='datumSquare_table'>{datum.vin_number}</td>
+                  <td className='datumSquare_table'>{datum.distance}(mi)</td>
+                  <td className='datumSquare_table'>${datum.tolls}</td>
+                  <td className='datumSquare_table'>${datum.foodBudget}</td>
+                  <td className='datumSquare_table'>${datum.wearAndTear}</td>
+                  <td className='datumSquare_table'>${datum.misc}</td>
+                  <td className='datumSquare_table'>{datum.startingPoint}</td>
+                  <td className='datumSquare_table'>{datum.endingPoint}</td>
+         
+                </tr>
+              );
+            })}
           </tbody>
           <tfoot></tfoot>
         </table>
